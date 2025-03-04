6c8a640f16b243fd23d19e61ab00d658aac90f3f
VictorGabriel13 <victorgabrieldelimarocha@gmail.com>
2025-03-04 02:05:50 -0300

    Primeiro commit do meu Portifolio

---
+++ index.js
@@ -0,0 +1,15 @@
+Aliaca();
+TeAmo();
+
+function Aliaca() {
+        console.log("Inicializando o Programa....")
+        console.log("Véu e grinalda, lua de mel.");
+        console.log("Chuva de arroz e tudo depois.");
+        console.log("Dama de honra, pega o buquê.");
+        console.log("Ninguém mais feliz que eu e você.");
+    }
+function TeAmo() {
+        for(let Ruiva = 1; Ruiva <=10; Ruiva ++) {
+            console.log("Te Amo ❤️");
+        }
+    }
